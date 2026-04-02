eae69b577f1417bf9ebd664f626f70186a767374
EAF <146513769+pocongtobat008@users.noreply.github.com>
2026-04-02 04:53:50 +0000

    update port 5005

--- vite.config.js
+++ vite.config.js
@@ -24,17 +24,17 @@ export default defineConfig({
     ],
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000',
+        target: 'http://127.0.0.1:5005',
         changeOrigin: true,
         secure: false,
       },
       '/uploads': {
-        target: 'http://127.0.0.1:5000',
+        target: 'http://127.0.0.1:5005',
         changeOrigin: true,
         secure: false,
       },
       '/socket.io': {
-        target: 'http://127.0.0.1:5000',
+        target: 'http://127.0.0.1:5005',
         ws: true,
         changeOrigin: true,
         secure: false,
