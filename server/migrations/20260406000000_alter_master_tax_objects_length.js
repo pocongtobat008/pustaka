9dbd6c7b223575a1799f2803873cd92a7945f7bc
EAF <146513769+pocongtobat008@users.noreply.github.com>
2026-04-06 01:41:03 +0000

    update alt master db

--- server/migrations/20260406000000_alter_master_tax_objects_length.js
+++ server/migrations/20260406000000_alter_master_tax_objects_length.js
@@ -1,4 +1,4 @@
-exports.up = function(knex) {
+export const up = function(knex) {
   return knex.schema.alterTable('master_tax_objects', function(table) {
     // Mengubah tipe kolom menjadi text agar dapat menampung string yang sangat panjang
     table.text('name').alter();
@@ -6,7 +6,7 @@ exports.up = function(knex) {
   });
 };
 
-exports.down = function(knex) {
+export const down = function(knex) {
   return knex.schema.alterTable('master_tax_objects', function(table) {
     table.string('name', 255).alter();
     table.string('note', 255).alter();
